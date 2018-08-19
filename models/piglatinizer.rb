08cda0c6dfa08667aa727db22d45f4d2d8320264
Christine Xiang <yutingcxiang@gmail.com>
2018-08-19 22:41:06 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,19 +3,18 @@ class PigLatinizer
   
   def latinize(text)
     words = text.split(" ")
-    words[0][0]
-    # words.collect do |word|
-    #   if word[0].match(/\b[aeiouAEIOU]/)
-    #     word + "way"
-    #   else
-    #     word = word[1..-1] + word[0]
-    #     if word[0].match(/\b[aeiouAEIOU]/) 
-    #       word + "ay"
-    #     else 
-    #       word = word[1..-1] + word[0] + "ay"
-    #     end
-    #   end
-    # end
-    # words.join(" ")
+    words.collect do |word|
+      if word[0].match(/\b[aeiouAEIOU]/)
+        word + "way"
+      else
+        word = word[1..-1] + word[0]
+        if word[0].match(/\b[aeiouAEIOU]/) 
+          word + "ay"
+        else 
+          word = word[1..-1] + word[0] + "ay"
+        end
+      end
+    end
+    words.join(" ")
   end
 end
